b8445ab88fe1cc893a79ad148a18562bef2ce6a5
haupv <phamvanhau123321@gmail.com>
2019-09-15 00:01:05 +0700

    first cm

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+var status = 'on';
+app.get('/', function (req, res) {
+  if(status=='on'){
+    status = 'off';
+  }else{
+    status = 'on';
+  }
+  res.send(status)
+})
+
+app.listen(3000)
