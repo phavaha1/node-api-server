7aaf1af28885935cfc8d63dd915bda16ac44c830
haupv <phamvanhau123321@gmail.com>
2019-09-15 11:12:24 +0700

    acccept source sharing

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
 var status = 'on';
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static(__dirname + '/js'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/html/home.html'));
+})
+
 app.get('/status', function (req, res) {
-  res.send(status);
+  res.status(200);
+  res.json({"status": status});
 })
 
 app.post('/status', function (req, res) {
