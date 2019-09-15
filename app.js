57fbb7c1db35caafb25d14db60a30a455b4ffd56
haupv <phamvanhau123321@gmail.com>
2019-09-15 11:09:55 +0700

    post request

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var express = require('express')
 var app = express()
 
 var status = 'on';
-app.get('/', function (req, res) {
-  if(status=='on'){
+app.get('/status', function (req, res) {
+  res.send(status);
+})
+
+app.post('/status', function (req, res) {
+  if(status == 'on'){
     status = 'off';
   }else{
     status = 'on';
   }
-  res.send(status)
+  res.send('Status updated');
 })
 
 app.listen(process.env.PORT || 5000)
